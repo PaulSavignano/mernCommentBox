767d38f9830e9dd3c2986af08643584f2ee8c0fa
Paul Savignano <Paul.Savignano@gmail.com>
2017-01-05 18:43:46 -0800

    Add mongodb and routes.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 'use strict'
 
 // Dependencies
-import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const config = require('./config');
+const Comment = require('./model/comments');
 
 // Create instances
 const app = express();
@@ -11,12 +13,13 @@ const router = express.Router();
 
 // Set port
 const port = process.env.API_PORT || 3001;
+mongoose.connect(config.dbUri);
 
 // Configure API to use body-parser
-app.use(bodyParser.urlencode({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Configure CORS 
+// Configure CORS
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
@@ -30,6 +33,29 @@ app.use(function(req, res, next) {
 router.get('/', function(req, res) {
   res.json({ message: 'API Initialized!' });
 });
+
+router.route('/comments')
+  .get(function(req, res) {
+    Comment.find(function(err, comments) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json(comments);
+      }
+    });
+  })
+  .post(function(req, res) {
+    const comment = new Comment();
+    comment.author = req.body.author;
+    comment.text = req.body.text;
+    comment.save(function(err) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.json({ message: 'Comment successfully added!' });
+      }
+    });
+  });
 
 app.use('/api', router);
 
